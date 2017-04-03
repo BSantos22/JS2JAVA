d255ee642c6f7a51d87ac79cd8d6504f6045e823
Bruno Santos <bruno.santos1996@gmail.com>
2017-04-03 01:59:07 +0100

    variable declaration type inference working

--- src/js2java/JS2Java.java
+++ src/js2java/JS2Java.java
@@ -1,21 +1,33 @@
 package js2java;
 
 import java.io.File;
+import java.util.Scanner;
 
 import com.google.gson.JsonObject;
 
 public class JS2Java {
 	public static void main(String args[]) {
+		String varsFilepath;
+		String jsFilepath;
+		
 		if (args.length != 2) {
-			System.err.println("Usage: java JS2Java <js_code_file> <var_type_file>");
-			return;
+			Scanner reader = new Scanner(System.in);
+			System.out.println("Javascript code file name: ");
+			jsFilepath = reader.nextLine();
+			System.out.println("Variable types file name: ");
+			varsFilepath = reader.nextLine();
+			reader.close();
+		}
+		else {
+			jsFilepath = args[0];
+			varsFilepath = args[1];
 		}
 		
-		File jsFile = new File(args[0]);
+		File jsFile = new File("files/" + jsFilepath + ".json");
 		JsonReader parser = new JsonReader();
 		JsonObject js = parser.parse(jsFile);
 		
-		File varsFile = new File(args[1]);
+		File varsFile = new File("files/" + varsFilepath + ".json");
 		JsonObject vars = parser.parse(varsFile);
 		
 		TypeInferrer inferrer = new TypeInferrer(vars);
--- src/js2java/TypeInferrer.java
+++ src/js2java/TypeInferrer.java
@@ -1,6 +1,8 @@
 package js2java;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import com.google.gson.JsonArray;
@@ -10,18 +12,25 @@ import com.google.gson.JsonPrimitive;
 
 public class TypeInferrer {
 	private Map<String, String> vars;
+	private Map<String, ArrayList<JsonObject>> unknown_types;
 	
 	public TypeInferrer(JsonObject vars) {
 		this.vars = createVarsMap(vars);
 	}
 	
 	public void addTypes(JsonObject js) {
+		unknown_types = new HashMap<String, ArrayList<JsonObject>>();
 		addKnownTypes(js);
-		
-		//inferTypes(js);
+		if (!unknown_types.isEmpty()) {
+			inferTypes(js);
+		}
+		else {
+			System.out.println("No variable types to infer");
+		}
 	}
 	
-	private void addKnownTypes(JsonObject js) {
+	private String addKnownTypes(JsonObject js) {
+		String no_type = null;
 		
 		for (Map.Entry<String, JsonElement> entry: js.entrySet()) {
 			if (entry.getKey().equals("type") && entry.getValue().getAsString().equals("Identifier")) {
@@ -29,15 +38,30 @@ public class TypeInferrer {
 				if (vars.containsKey(var)) {
 					entry.setValue(new JsonPrimitive(vars.get(var)));
 				}
+				else {
+					no_type = var;
+				}
 			}
 			
 			if (entry.getValue() instanceof JsonArray) {
 				addKnownTypes((JsonArray) entry.getValue());
 			}
 			else if (entry.getValue() instanceof JsonObject) {
-				addKnownTypes((JsonObject) entry.getValue());
+				String r = addKnownTypes((JsonObject) entry.getValue());
+				if (r != null) {
+					if (unknown_types.containsKey(r)) {
+						unknown_types.get(r).add(js);
+					}
+					else {
+						ArrayList<JsonObject> a = new ArrayList<JsonObject>();
+						a.add(js);
+						unknown_types.put(r, a);
+					}
+				}
 			}
 		}
+		
+		return no_type;
 	}
 	
 	private void addKnownTypes(JsonArray list) {
@@ -61,6 +85,121 @@ public class TypeInferrer {
 	}
 	
 	private void inferTypes(JsonObject js) {
+		Iterator<String> it = unknown_types.keySet().iterator();
 		
+		while(it.hasNext()) {
+			String key = it.next();
+			ArrayList<JsonObject> op = unknown_types.get(key);
+			
+			String type = inferVariable(key, op);			
+			if (type != null) {
+				vars.put(key, type);
+				addKnownTypes(js);
+				unknown_types.remove(key);
+				System.out.println(key + " - " + type);
+			}
+			
+			if (!it.hasNext()) {
+				it = unknown_types.keySet().iterator();
+			}
+		}
+	}
+	
+	private String inferVariable(String var_name, ArrayList<JsonObject> expr) {
+		ArrayList<String> possible_types = new ArrayList<String>();
+		
+		for (int i = 0; i < expr.size(); i++) {
+			String type = expr.get(i).getAsJsonObject().get("type").getAsString();
+			
+			switch (type) {
+			case "VariableDeclarator":
+				ArrayList<String> vd = new ArrayList<String>();
+				JsonElement init = expr.get(i).getAsJsonObject().get("init");
+				if (init.isJsonObject()) {
+					vd = getTypes(var_name, init.getAsJsonObject());
+				}
+				if (vd == null) {
+					return null;
+				}
+				else {
+					possible_types.addAll(vd);
+				}
+				break;
+			case "AssignmentExpression":
+			
+				break;
+			}
+			
+		}
+		
+		return getMaxType(possible_types);
+	}
+	
+	private ArrayList<String> getTypes(String var_name, JsonObject expr) {
+		ArrayList<String> types = new ArrayList<String>();
+		
+		for (Map.Entry<String, JsonElement> entry: expr.entrySet()) {
+			if (entry.getValue().isJsonObject()) {
+				ArrayList<String> a = getTypes(var_name, (JsonObject) entry.getValue());
+				if (a == null) {
+					return null;
+				}
+				else {
+					types.addAll(a);
+				}
+			}
+			else if (entry.getKey().equals("type")) {
+				if (entry.getValue().getAsString().equals("Identifier") && !expr.get("name").getAsString().equals(var_name)) {
+					return null;
+				}
+				else if (entry.getValue().getAsString().equals("Literal")) {
+					String v = expr.get("raw").getAsString();
+			
+					if (v.equals("true") || v.equals("false")) {
+						types.add("boolean");
+					}
+					else if (v.contains(".")) {
+						types.add("double");
+					}
+					else {
+						types.add("int");
+					}
+				}
+				else if (expr.has("name")){
+					types.add(entry.getValue().getAsString());
+				}
+			}
+		}
+	
+		return types;
+	}
+	
+	private String getMaxType(ArrayList<String> types) {
+		if (types.contains("double")) {
+			return "double";
+		}
+		if (types.contains("float")) {
+			return "float";
+		}
+		if (types.contains("long")) {
+			return "long";
+		}
+		if (types.contains("int")) {
+			return "int";
+		}
+		if (types.contains("short")) {
+			return "short";
+		}
+		if (types.contains("char")) {
+			return "char";
+		}
+		if (types.contains("byte")) {
+			return "byte";
+		}
+		if (types.contains("boolean")) {
+			return "boolean";
+		}
+		
+		return null;
 	}
 }
