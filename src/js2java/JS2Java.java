e1e2950c6193a650f27798b259531739e02575d0
dfreddy <fred.p.p.r@gmail.com>
2017-04-26 10:04:37 +0100

    Trying to read .json files from directories
    devia dar mas nao dá por alguma razao

--- src/js2java/JS2Java.java
+++ src/js2java/JS2Java.java
@@ -17,6 +17,7 @@ public class JS2Java {
 			Scanner reader = new Scanner(System.in);
 			System.out.println("Javascript code file name: ");
 			jsFilepath = reader.nextLine();
+			jsFilepath += ".json";
 			System.out.println("Variable types file name: ");
 			varsFilepath = reader.nextLine();
 			reader.close();
@@ -26,9 +27,21 @@ public class JS2Java {
 			varsFilepath = args[1];
 		}
 		
+		String dir = checkFileDirectory(jsFilepath);
+		if(dir == "") {
+			System.out.println("\nCouldn't find such a file");
+			return;
+		}
+		
+		File jsFile = new File(dir + "/" + jsFilepath);
+		JsonReader parser = new JsonReader();
+		JsonObject js = parser.parse(jsFile);
+		
+		/*
 		File jsFile = new File("files/" + jsFilepath + ".json");
 		JsonReader parser = new JsonReader();
 		JsonObject js = parser.parse(jsFile);
+		*/
 		
 		File varsFile = new File("files/" + varsFilepath + ".json");
 		JsonObject vars = parser.parse(varsFile);
@@ -44,5 +57,33 @@ public class JS2Java {
 		JsonObject vars = parser.parse(new File(typesFile));
 		TypeInferrer inferrer = new TypeInferrer(vars);
 		return inferrer.addTypes(js);
+	}
+	
+	public static String checkFileDirectory(String filename) {
+		System.out.println("\n\tChecking for " + filename + "\n");
+		String dir = "";
+		File files = new File("files");
+		File[] filesFolders = files.listFiles();
+		File[] jsonFiles;
+		
+		for(int i=0; i < filesFolders.length; i++) {
+			if(filesFolders[i].isDirectory()) {
+				System.out.println("\n" + filesFolders[i].getName() + " is directory");
+				jsonFiles = filesFolders[i].listFiles();
+				
+				for(int k=0; k < jsonFiles.length; k++) {
+					System.out.println(jsonFiles[k].getName() + " tested");
+					
+					if(jsonFiles[k].getName() == filename) {
+						dir = filesFolders[i].getName();
+						System.out.println("\tFound");
+						return dir;
+					}
+				}
+			}
+		}
+		
+		
+		return "";
 	}
 }
